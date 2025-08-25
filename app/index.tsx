79c896d0fb9522ad4f7f41a509a05142b3845577
Killian KOPP <killiankopp@gmail.com>
2025-08-25 11:54:33 +0200

    before tabs

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,34 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={styles.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text style={styles.text}>Hello World !</Text>
+      <Link href="/about" style={styles.link}>
+        Go to About
+      </Link>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#25292e",
+  },
+  link: {
+    marginTop: 20,
+    fontSize: 18,
+    color: "#1e90ff",
+  },
+});
