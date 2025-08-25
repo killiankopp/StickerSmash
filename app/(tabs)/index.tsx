5c40c3f6269b9aa34c6f06a47d151aac05c0b852
Killian KOPP <killiankopp@gmail.com>
2025-08-25 12:23:22 +0200

    tabs

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,3 @@
-import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
@@ -7,9 +6,6 @@ export default function Index() {
       style={styles.container}
     >
       <Text style={styles.text}>Hello World !</Text>
-      <Link href="/about" style={styles.link}>
-        Go to About
-      </Link>
     </View>
   );
 }
